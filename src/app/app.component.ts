461e52ce8d40a2d6e07f5a16e9519d0d44be09bb
andrikash <dizhakandrik@gmail.com>
2018-10-29 20:49:13 +0200

    Yes Armin, here we are

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  carText = '';
+  inputText = '';
+  onKeyUp(event : Event){
+  this.inputText = (<HTMLInputElement>event.target).value;
+    // console.log(this.inputText);
+  }
+  addCar(){
+    this.carText = 'helloooo';
+  }
   constructor() {
 
   }
