135d812b03f2f94b0b4698692845938e31e0bd4e
andrikash <dizhakandrik@gmail.com>
2018-10-29 18:59:16 +0200

    Initial commit

---
+++ src/app/car/car.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-car',
+  templateUrl: './car.component.html',
+  styleUrls: ['./car.component.scss']
+})
+export class CarComponent {
+}
