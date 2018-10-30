21c94acced648362db93c6013adbbaedcb4a42b0
andrikash <dizhakandrik@gmail.com>
2018-10-30 10:01:20 +0200

    One way binding.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   carText = '';
   inputText = '';
-  onKeyUp(event: Event) {
-  this.inputText = (<HTMLInputElement>event.target).value;
-    // console.log(this.inputText);
+  onKeyUp(value) {
+    this.inputText = value;
   }
   addCar() {
     this.carText = 'helloooo';
   }
+
   constructor() {
 
   }
